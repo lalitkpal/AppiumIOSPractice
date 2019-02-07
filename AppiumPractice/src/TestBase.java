89e58814484f9ddf4d4e6899931591732b9f48d8
lalitkpal <lalit.k.pal@gmail.com>
2019-02-08 01:57:28 +0530

    Committing issues faced and update testbase working code
    Change-Id: Iba90fe71def6965c72cbb98089117e81576f9e2b

--- AppiumPractice/src/TestBase.java
+++ AppiumPractice/src/TestBase.java
@@ -3,7 +3,8 @@ import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
 
@@ -20,14 +21,14 @@ public class TestBase {
 		
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.1");
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
 		//Following capability required for ios version greater than 8
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
 		cap.setCapability(MobileCapabilityType.APP, "//users//Lalit//Desktop//UICatalog.app");
 	
 		
-		AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-		
+		IOSDriver<IOSElement> driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
 		
 		
 	}
