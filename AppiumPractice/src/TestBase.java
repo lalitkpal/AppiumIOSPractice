d650f46879bc31aae13be329eade5581adcbfa31
lalitkpal <lalit.k.pal@gmail.com>
2019-02-12 01:29:42 +0530

    Changes related to absolute app path

--- AppiumPractice/src/TestBase.java
+++ AppiumPractice/src/TestBase.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -19,13 +20,16 @@ public class TestBase {
 	
 	public static void main(String args[]) throws MalformedURLException {
 		
+		File appDir = new File("src");
+		File app = new File(appDir, "UICatalog.app");
+		
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.1");
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
 		//Following capability required for ios version greater than 8
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
-		cap.setCapability(MobileCapabilityType.APP, "/users/Lalit/Desktop/UICatalog.app");
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 	
 		
 		IOSDriver<IOSElement> driver = new IOSDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
