f65051ce98158366e6d0c94eeaaa5c42294ff7a1
lalitkpal <lalit.k.pal@gmail.com>
2019-02-14 22:46:58 +0530

    Adding testPractice file

---
+++ AppiumPractice/src/testPractice.java
@@ -0,0 +1,15 @@
+import java.net.MalformedURLException;
+
+public class testPractice extends TestBase {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		driver = Setup();
+		
+		driver.findElementByAccessibilityId("Alert Views").click();
+		
+
+	}
+
+}
